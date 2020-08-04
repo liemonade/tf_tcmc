6e4d7eb5c5e1d21d7d43bdad2cba9a432ddcb909
Darvin Mertsch <darvin@mertsch.im>
2020-08-04 21:38:49 +0200

    Implement utility layer to convert concatened variable-length sequences into a zero-padded batch of sequences.

--- tcmc/tensor_utils.py
+++ tcmc/tensor_utils.py
@@ -39,7 +39,7 @@ def crosscat_matrices(A,B):
 def broadcast_matrix_indices_to_tensor_indices(matrix_indices, tensor_shape):
     """Broadcast matrix indices to arbitrary tensor shapes
     
-    This can be used for functions like `tf.scatter_nd` to propate matrix-wise actions into tensors of matrices.
+    This can be used for functions like `tf.scatter_nd` to propagate matrix-wise actions into tensors of matrices.
     """
     
     if len(tensor_shape) < 2:
@@ -75,3 +75,148 @@ def segment_ids(segment_lengths):
     
     # Use accumulated sums to generate ids for every segment
     return tf.cumsum(inc_ids)
+
+
+
+def batched_segment_indices(segment_lengths):
+    """
+    Matrix of batch indices
+    
+    Example:
+    
+    >>> batched_segment_indices([1,3,2])
+    <tf.Tensor: shape=(6, 2), dtype=int32, numpy=
+    array([[0, 0],
+           [1, 0],
+           [1, 1],
+           [1, 2],
+           [2, 0],
+           [2, 1]], dtype=int32)>
+    
+    Args:
+        segment_lengths (List[int]) Length of batch entries. The number `len(segment_lengths)` 
+            represents the number of batches.
+    
+    
+    Returns:
+        List[List[int]] A matrix of shape `(sum(segment_lengths), 2)` rows and two columns.
+    
+    """
+    # determine the indices where the value of segment_ids change
+    inc_idx = tf.cumsum(segment_lengths)
+    total_length = inc_idx[-1]
+    inc_idx = inc_idx[:-1]
+    
+    subs = tf.scatter_nd(tf.expand_dims(inc_idx, 1), segment_lengths[:-1], [total_length])
+    
+    # Insert ones on the positions where the index should change
+    inc_ids = tf.scatter_nd(tf.expand_dims(inc_idx, 1), tf.ones_like(inc_idx), [total_length])
+    
+    # Use accumulated sums to generate ids for every segment
+    seg_ids = tf.cumsum(inc_ids)
+    
+    # determine the sequence of ranges
+    batch_indices = tf.cumsum(tf.ones([total_length],dtype=segment_lengths.dtype) - subs) - 1
+    
+    return tf.stack((seg_ids, batch_indices), axis=1)
+
+
+
+
+
+class BatchedSequences(tf.keras.layers.Layer):
+    """
+    Zero-padded batches of sequences from concatenated variable-length sequences
+    
+    Example:
+    
+    >>> feature_size = 4
+    >>> num_characters = 6
+    >>> sequence_lengths = [2,1,3]
+    >>> concatenated_sequences = tf.reshape(
+    >>>     tf.range(num_characters*feature_size), 
+    >>>     shape=(num_sequences,feature_size))
+    >>> # We have 3 sequences of variable length with 4-dimensional "characters". 
+    >>> # The first two rows ("characters") should belong to the first sequence, 
+    >>> # the third row to the second sequence and the last three rows should 
+    >>> # belong to the third sequence.
+    >>> print(concatenated_sequences)
+    tf.Tensor(
+    [[ 0  1  2  3]
+     [ 4  5  6  7]
+     [ 8  9 10 11]
+     [12 13 14 15]
+     [16 17 18 19]
+     [20 21 22 23]], shape=(6, 4), dtype=int32)
+    >>> bs_layer = BatchedSequences(feature_size = feature_size, dtype=tf.float64)
+    >>> bs = bs_layer([concatenated_sequences, sequence_lengths])
+    >>> print(bs)
+    >>> # The output is a 3-tensor of shape (num_sequences, max_sequence_length, feature_size),
+    >>> # where `max_sequence_length` is given by `max(sequence_lengths)` and
+    >>> # `num_sequences` is given by `len(sequence_lengths)`.
+    tf.Tensor(
+    [[[ 0.  1.  2.  3.]
+      [ 4.  5.  6.  7.]
+      [ 0.  0.  0.  0.]]
+
+     [[ 8.  9. 10. 11.]
+      [ 0.  0.  0.  0.]
+      [ 0.  0.  0.  0.]]
+
+     [[12. 13. 14. 15.]
+      [16. 17. 18. 19.]
+      [20. 21. 22. 23.]]], shape=(3, 3, 4), dtype=float64)
+    
+    
+    Args:
+        feature_size (int) (Optional) Dimension of the character-space [should be provided if this layer is used in a `tf.keras.model`]
+
+    Returns:
+        A `Tensor` of `dtype`: The 3-Tensor discussed above (see the example).
+    """
+    def __init__(self, feature_size = None, **kwargs):
+        super(BatchedSequences, self).__init__(**kwargs)
+        self.feature_size = feature_size
+
+    def build(self, input_shape):
+        super(BatchedSequences, self).build(input_shape)
+
+    @tf.function
+    def call(self, inputs, training = None):
+        
+        # concatenated sequences 
+        S = inputs[0]
+        
+        # the lengths of the respective sequences
+        sl = inputs[1]
+        
+        # the raw keras inputs inforce shape [None,1].
+        # get rid of the unwanted dimension
+        sls = tf.reshape(sl, shape = [-1])
+        
+        
+        #max_sl = tf.cast(tf.math.reduce_max(sls), dtype=tf.int32)
+        max_sl = tf.math.reduce_max(sls)
+        num_sequences = tf.shape(sls)[0]
+        feature_size = tf.shape(S)[-1] if self.feature_size == None else self.feature_size
+        
+        
+        with tf.name_scope("batched_segment_indices"):
+            batch_indices = batched_segment_indices(sls)
+            
+        batched_P_shape = (num_sequences, max_sl, feature_size)
+    
+    
+        batched_sequences = tf.cast(tf.scatter_nd(batch_indices, S, batched_P_shape), dtype=self.dtype)
+        
+        
+        return batched_sequences
+        
+    def get_config(self):
+        base_config = super(BatchedSequences, self).get_config()
+        base_config['feature_size'] = self.feature_size
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
